a5a2667955d902b3ab9c8bd9084007103b6cab15
Paul Chavard <git@paul.chavard.net>
2020-01-03 19:22:10 +0100

    Add basic tests

---
+++ test/strategies/backup-strategy-test.ts
@@ -0,0 +1,251 @@
+import { BackupStrategy } from '../../src/index';
+import Coordinator from '@orbit/coordinator';
+import {
+  Source,
+  Transform,
+  TransformBuilder,
+  syncable,
+  buildTransform,
+  pullable
+} from '@orbit/data';
+import { Exception } from '@orbit/core';
+
+const { module, test } = QUnit;
+
+module('BackupStrategy', function(hooks) {
+  const t = new TransformBuilder();
+  const tA = buildTransform(
+    [t.addRecord({ type: 'planet', id: 'a', attributes: { name: 'a' } })],
+    null,
+    'a'
+  );
+  const tB = buildTransform(
+    [t.addRecord({ type: 'planet', id: 'b', attributes: { name: 'b' } })],
+    null,
+    'b'
+  );
+
+  let strategy: BackupStrategy;
+  let coordinator: Coordinator;
+  let s1: any;
+  let s2: any;
+
+  hooks.beforeEach(function() {
+    @syncable
+    @pullable
+    class MySource extends Source {
+      _pull() {
+        return { operations: [] };
+      }
+      _sync() {}
+    }
+
+    s1 = new MySource({ name: 's1' });
+    s2 = new MySource({ name: 's2' });
+  });
+
+  test('can be instantiated', function(assert) {
+    strategy = new BackupStrategy({ source: 's1', target: 's2' });
+
+    assert.ok(strategy);
+    assert.strictEqual(
+      strategy.blocking,
+      false,
+      'blocking is false by default'
+    );
+    assert.equal(
+      strategy.name,
+      's1:transform -> s2:sync',
+      'name is based on source names by default'
+    );
+  });
+
+  test('assigns source and target when activated', async function(assert) {
+    strategy = new BackupStrategy({ source: 's1', target: 's2' });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    await coordinator.activate();
+
+    assert.strictEqual(strategy.source, s1, 'source is set');
+    assert.strictEqual(strategy.target, s2, 'target is set');
+  });
+
+  test('installs listeners on activate and removes them on deactivate', async function(assert) {
+    assert.expect(6);
+
+    strategy = new BackupStrategy({ source: 's1', target: 's2' });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    assert.equal(
+      s1.listeners('transform').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s2.listeners('transform').length,
+      0,
+      'no listeners installed yet'
+    );
+
+    await coordinator.activate();
+
+    assert.equal(s1.listeners('transform').length, 1, 'listeners installed');
+    assert.equal(
+      s2.listeners('transform').length,
+      0,
+      'no listeners installed on target'
+    );
+
+    await coordinator.deactivate();
+
+    assert.equal(s1.listeners('transform').length, 0, 'listeners removed');
+    assert.equal(
+      s2.listeners('transform').length,
+      0,
+      'still no listeners on target'
+    );
+  });
+
+  test('observes source `transform` event and invokes `sync` on target', async function(assert) {
+    assert.expect(2);
+
+    strategy = new BackupStrategy({ source: 's1', target: 's2' });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    s2._sync = async function(transform: Transform): Promise<void> {
+      assert.strictEqual(
+        transform,
+        tA,
+        'argument to _sync is expected Transform'
+      );
+      assert.strictEqual(this, s2, 'context is that of the target');
+    };
+
+    await coordinator.activate();
+    await s1.transformed([tA]);
+  });
+
+  test('can apply a `filter` function', async function(assert) {
+    assert.expect(4);
+
+    strategy = new BackupStrategy({
+      source: 's1',
+      target: 's2',
+      filter(transform: Transform): boolean {
+        assert.strictEqual(this, strategy, 'context is the strategy');
+        return transform === tB;
+      }
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    s2._sync = async function(transform: Transform): Promise<void> {
+      assert.strictEqual(
+        transform,
+        tB,
+        'argument to _sync is expected Transform'
+      );
+      assert.strictEqual(this, s2, 'context is that of the target');
+    };
+
+    await coordinator.activate();
+    await s1.transformed([tA, tB]);
+  });
+
+  test('can catch errors with a `catch` function', async function(assert) {
+    assert.expect(6);
+
+    strategy = new BackupStrategy({
+      source: 's1',
+      target: 's2',
+      blocking: true,
+      catch(e: Exception, transform: Transform) {
+        assert.equal(e.message, ':(', 'error matches');
+        assert.strictEqual(
+          transform,
+          tA,
+          'argument to catch is expected Transform'
+        );
+        assert.strictEqual(this, strategy, 'context is the strategy');
+      }
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    s2._sync = async function(transform: Transform): Promise<void> {
+      assert.strictEqual(
+        transform,
+        tA,
+        'argument to _sync is expected Transform'
+      );
+      assert.strictEqual(this, s2, 'context is that of the target');
+      throw new Error(':(');
+    };
+
+    await coordinator.activate();
+    await s1.transformed([tA]);
+
+    assert.ok(true, 'transform event settled');
+  });
+
+  test('errors rethrown within a `catch` function are not propagated', async function(assert) {
+    assert.expect(6);
+
+    strategy = new BackupStrategy({
+      source: 's1',
+      target: 's2',
+      blocking: true,
+      catch(e: Exception, transform: Transform) {
+        assert.equal(e.message, ':(', 'error matches');
+        assert.strictEqual(
+          transform,
+          tA,
+          'argument to catch is expected Transform'
+        );
+        assert.strictEqual(this, strategy, 'context is the strategy');
+        throw e;
+      }
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    s2._sync = function(transform: Transform): Promise<void> {
+      assert.strictEqual(
+        transform,
+        tA,
+        'argument to _sync is expected Transform'
+      );
+      assert.strictEqual(this, s2, 'context is that of the target');
+      throw new Error(':(');
+    };
+
+    await coordinator.activate();
+    await s1.transformed([tA]);
+
+    assert.ok(
+      true,
+      'transformed event still settled after error was caught and rethrown'
+    );
+  });
+});
---
+++ test/strategies/pessimistic-strategy-test.ts
@@ -0,0 +1,383 @@
+import { PessimisticStrategy } from '../../src/index';
+import Coordinator from '@orbit/coordinator';
+import {
+  Source,
+  Transform,
+  TransformBuilder,
+  pushable,
+  updatable,
+  buildTransform
+} from '@orbit/data';
+
+const { module, test } = QUnit;
+
+module('PessimisticStrategy', function(hooks) {
+  const t = new TransformBuilder();
+  const tA = buildTransform(
+    [t.addRecord({ type: 'planet', id: 'a', attributes: { name: 'a' } })],
+    null,
+    'a'
+  );
+  const tB = buildTransform(
+    [t.addRecord({ type: 'planet', id: 'b', attributes: { name: 'b' } })],
+    null,
+    'b'
+  );
+
+  let strategy: PessimisticStrategy;
+  let coordinator: Coordinator;
+  let s1: any;
+  let s2: any;
+
+  hooks.beforeEach(function() {
+    @pushable
+    @updatable
+    class MySource extends Source {}
+
+    s1 = new MySource({ name: 's1' });
+    s2 = new MySource({ name: 's2' });
+  });
+
+  test('can be instantiated', function(assert) {
+    strategy = new PessimisticStrategy({
+      source: 's1',
+      target: 's2'
+    });
+
+    assert.ok(strategy);
+    assert.strictEqual(
+      strategy.passHints,
+      true,
+      'passHints is true by default'
+    );
+    assert.equal(
+      strategy.name,
+      's1:{query|update} -> s2:{pull|push}',
+      'name is based on source names by default'
+    );
+  });
+
+  test('assigns source and target when activated', async function(assert) {
+    strategy = new PessimisticStrategy({
+      source: 's1',
+      target: 's2'
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    await coordinator.activate();
+    assert.strictEqual(strategy.source, s1, 'source is set');
+    assert.strictEqual(strategy.target, s2, 'target is set');
+  });
+
+  test('installs listeners on activate and removes them on deactivate', async function(assert) {
+    assert.expect(21);
+
+    strategy = new PessimisticStrategy({
+      source: 's1',
+      target: 's2'
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    assert.equal(
+      s1.listeners('beforeQuery').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s1.listeners('beforeUpdate').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s1.listeners('transform').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s1.listeners('queryFail').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s1.listeners('updateFail').length,
+      0,
+      'no listeners installed yet'
+    );
+
+    assert.equal(
+      s2.listeners('beforeUpdate').length,
+      0,
+      'no listeners installed yet'
+    );
+    assert.equal(
+      s2.listeners('transform').length,
+      0,
+      'no listeners installed yet'
+    );
+
+    await coordinator.activate();
+
+    assert.equal(s1.listeners('beforeQuery').length, 1, 'listeners installed');
+    assert.equal(s1.listeners('beforeUpdate').length, 1, 'listeners installed');
+    assert.equal(s1.listeners('queryFail').length, 1, 'listeners installed');
+    assert.equal(s1.listeners('updateFail').length, 1, 'listeners installed');
+
+    assert.equal(
+      s1.listeners('transform').length,
+      0,
+      'no transform listener installed on source'
+    );
+
+    assert.equal(
+      s2.listeners('beforeUpdate').length,
+      0,
+      'no listeners installed on target'
+    );
+    assert.equal(s2.listeners('transform').length, 1, 'listeners installed');
+
+    await coordinator.deactivate();
+
+    assert.equal(s1.listeners('beforeQuery').length, 0, 'listeners removed');
+    assert.equal(s1.listeners('beforeUpdate').length, 0, 'listeners removed');
+    assert.equal(s1.listeners('queryFail').length, 0, 'listeners removed');
+    assert.equal(s1.listeners('updateFail').length, 0, 'listeners removed');
+
+    assert.equal(
+      s1.listeners('transform').length,
+      0,
+      'still no transform listener on source'
+    );
+
+    assert.equal(
+      s2.listeners('beforeUpdate').length,
+      0,
+      'still no listeners on target'
+    );
+    assert.equal(
+      s2.listeners('transform').length,
+      0,
+      'trandform listener removed from target'
+    );
+  });
+
+  test('observes source `on` event and invokes `action` on target', async function(assert) {
+    assert.expect(3);
+
+    strategy = new PessimisticStrategy({
+      source: 's1',
+      target: 's2'
+    });
+
+    coordinator = new Coordinator({
+      sources: [s1, s2],
+      strategies: [strategy]
+    });
+
+    s1._update = async function(transform: Transform): Promise<any> {
+      assert.strictEqual(
+        transform,
+        tA,
+        'argument to _update is expected Transform'
+      );
+    };
+
+    s2._push = async function(transform: Transform): Promise<Transform[]> {
+      assert.strictEqual(
+        transform,
+        tA,
+        'argument to _push is expected Transform'
+      );
+      assert.strictEqual(this, s2, 'context is that of the target');
+      return [];
+    };
+
+    await coordinator.activate();
+    await s1.update(tA);
+  });
+
+  // test('with `passHints: true` and `blocking: true`, will pass `hints` that result from applying the target action', async function(assert) {
+  //   assert.expect(5);
+
+  //   strategy = new PessimisticStrategy({
+  //     source: 's1',
+  //     target: 's2',
+  //     on: 'beforeUpdate',
+  //     action: 'push',
+  //     blocking: true,
+  //     passHints: true
+  //   });
+
+  //   coordinator = new Coordinator({
+  //     sources: [s1, s2],
+  //     strategies: [strategy]
+  //   });
+
+  //   s1._update = async function(
+  //     transform: Transform,
+  //     hints: any
+  //   ): Promise<any> {
+  //     assert.strictEqual(
+  //       transform,
+  //       tA,
+  //       'argument to _update is expected Transform'
+  //     );
+  //     assert.deepEqual(hints.data, [tA], 'result is passed as a hint');
+  //   };
+
+  //   s2._push = async function(
+  //     transform: Transform,
+  //     hints: any
+  //   ): Promise<Transform[]> {
+  //     assert.deepEqual(hints, {}, 'no hints are passed to `push`');
+  //     assert.strictEqual(
+  //       transform,
+  //       tA,
+  //       'argument to _push is expected Transform'
+  //     );
+  //     assert.strictEqual(this, s2, 'context is that of the target');
+  //     return [tA];
+  //   };
+
+  //   await coordinator.activate();
+  //   await s1.update(tA);
+  // });
+
+  // test('can apply a `filter` function', async function(assert) {
+  //   assert.expect(4);
+
+  //   strategy = new RequestStrategy({
+  //     source: 's1',
+  //     target: 's2',
+  //     on: 'update',
+  //     action: 'push',
+  //     filter(transform: Transform): boolean {
+  //       assert.strictEqual(this, strategy, 'context is the strategy');
+  //       return transform === tB;
+  //     }
+  //   });
+
+  //   coordinator = new Coordinator({
+  //     sources: [s1, s2],
+  //     strategies: [strategy]
+  //   });
+
+  //   s1._update = async function(): Promise<any> {};
+
+  //   s2._push = async function(transform: Transform): Promise<Transform[]> {
+  //     assert.strictEqual(
+  //       transform,
+  //       tB,
+  //       'argument to _push is expected Transform'
+  //     );
+  //     assert.strictEqual(this, s2, 'context is that of the target');
+  //     return [];
+  //   };
+
+  //   await coordinator.activate();
+  //   await s1.update(tA);
+  //   await s1.update(tB);
+  // });
+
+  //   test('if `blocking` is a function it gets invoked with the query', async function(assert) {
+  //     assert.expect(5);
+
+  //     strategy = new RequestStrategy({
+  //       source: 's1',
+  //       target: 's2',
+  //       on: 'update',
+  //       action: 'push',
+  //       blocking(transform: Transform): boolean {
+  //         assert.ok(
+  //           this instanceof RequestStrategy,
+  //           'it is bound to the strategy'
+  //         );
+  //         assert.strictEqual(
+  //           transform,
+  //           tA,
+  //           'argument to _update is expected Transform'
+  //         );
+  //         return false;
+  //       }
+  //     });
+
+  //     coordinator = new Coordinator({
+  //       sources: [s1, s2],
+  //       strategies: [strategy]
+  //     });
+
+  //     s1._update = async function(transform: Transform): Promise<any> {
+  //       assert.strictEqual(
+  //         transform,
+  //         tA,
+  //         'argument to _update is expected Transform'
+  //       );
+  //     };
+
+  //     s2._push = async function(transform: Transform): Promise<Transform[]> {
+  //       assert.strictEqual(
+  //         transform,
+  //         tA,
+  //         'argument to _push is expected Transform'
+  //       );
+  //       assert.strictEqual(this, s2, 'context is that of the target');
+  //       return [];
+  //     };
+
+  //     await coordinator.activate();
+  //     await s1.update(tA);
+  //   });
+
+  //   test('for events that are invoked with more than one arg, pass all args to any custom handler functions', async function(assert) {
+  //     assert.expect(9);
+
+  //     strategy = new RequestStrategy({
+  //       source: 's1',
+  //       on: 'updateFail',
+  //       async action(transform: Transform, e: Error): Promise<void> {
+  //         assert.ok(
+  //           this instanceof RequestStrategy,
+  //           '`action` is bound to the strategy'
+  //         );
+  //         assert.strictEqual(transform, tA, 'transform is passed to `action`');
+  //         assert.equal(e.message, ':(', 'error is passed to `action`');
+  //       },
+  //       blocking(transform: Transform, e: Error): boolean {
+  //         assert.ok(
+  //           this instanceof RequestStrategy,
+  //           '`blocking` is bound to the strategy'
+  //         );
+  //         assert.strictEqual(transform, tA, 'transform is passed to `blocking`');
+  //         assert.equal(e.message, ':(', 'error is passed to `blocking`');
+  //         return false;
+  //       },
+  //       filter(transform: Transform, e: Error): boolean {
+  //         assert.ok(
+  //           this instanceof RequestStrategy,
+  //           '`filter` is bound to the strategy'
+  //         );
+  //         assert.strictEqual(transform, tA, 'transform is passed to `filter`');
+  //         assert.equal(e.message, ':(', 'error is passed to `filter`');
+  //         return true;
+  //       }
+  //     });
+
+  //     coordinator = new Coordinator({
+  //       sources: [s1, s2],
+  //       strategies: [strategy]
+  //     });
+
+  //     await coordinator.activate();
+
+  //     s1.emit('updateFail', tA, new Error(':('));
+  //   });
+
+  //   // TODO - test blocking option
+});
