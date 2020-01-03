43cdec125d3fb80c44220543189b0c4111d4619f
Paul Chavard <git@paul.chavard.net>
2020-01-03 14:31:02 +0100

    fix build

--- Brocfile.js
+++ Brocfile.js
@@ -4,14 +4,22 @@ const build = require('@glimmer/build');
 const packageDist = require('@glimmer/build/lib/package-dist');
 
 let buildOptions = {
-  external: ['@orbit/utils', '@orbit/core', '@orbit/data']
+  external: [
+    '@orbit/utils',
+    '@orbit/core',
+    '@orbit/data',
+    '@orbit/coordinator',
+    '@orbit/record-cache'
+  ]
 };
 
 if (process.env.BROCCOLI_ENV === 'tests') {
   buildOptions.vendorTrees = [
     packageDist('@orbit/utils', { lang: 'es2017' }),
     packageDist('@orbit/core', { lang: 'es2017' }),
-    packageDist('@orbit/data', { lang: 'es2017' })
+    packageDist('@orbit/data', { lang: 'es2017' }),
+    packageDist('@orbit/coordinator', { lang: 'es2017' }),
+    packageDist('@orbit/record-cache', { lang: 'es2017' })
   ];
   buildOptions.test = { es5: false };
 }
